496fd4925c1276c1b0beb2d847afdb9094a25901
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-23 00:49:17 +0100

    working on topbar

--- src/topbar/Topbar.js
+++ src/topbar/Topbar.js
@@ -21,6 +21,7 @@ const Topbar = () => {
         <div className="topRight">
             <img className="topImg"
               src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="user" />
+                <i className="topSearchIcon fas fa-search"></i>
         </div>
         </div>
     )
