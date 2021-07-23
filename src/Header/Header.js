3098ee910f77ccfdf07862b423de93301bcd82a5
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-23 11:27:04 +0100

    working on topbar

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,8 +3,16 @@ import './header.css'
 const Header = () => {
     return (
         <div className="header">
-            
-        </div>
+      <div className="headerTitles">
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">BLOG</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
+      />
+    </div>
     )
 }
 
