ecf883910d982aa94d286432e503a7e3391ad627
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-23 12:45:27 +0100

    working on post

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Home from "./Pages/home/Home";
 import Topbar from "./components/topbar/Topbar";
+import Single from "./Pages/single/Single";
 
 function App() {
   return (
    <>
   <Topbar/>
-  <Home/>
+  <Single/>
    </>
   );
 }
