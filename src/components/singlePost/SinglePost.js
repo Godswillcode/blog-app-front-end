113be6fb24d56648573e9d2f7aefc05386fab28f
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-23 13:00:35 +0100

    working on post

--- src/components/singlePost/SinglePost.js
+++ src/components/singlePost/SinglePost.js
@@ -1,11 +1,18 @@
-import './singlePost.css'
+import "./singlePost.css";
 
 const singlePost = () => {
-    return (
-        <div className="singlePost">
-            single  Post
-        </div>
-    )
-}
+  return (
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        <img
+          className="singlePostImg"
+          src="https://images.pexels.com/photos/708392/pexels-photo-708392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          alt=""
+        />
+         <h1 className="singlePostTitle">Lorem ipsum dolor sit amet.</h1>
+      </div>
+    </div>
+  );
+};
 
-export default singlePost
+export default singlePost;
