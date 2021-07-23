1147bdb47bde463f57689f1e71850b3658000889
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-23 13:10:58 +0100

    working on post

--- src/components/singlePost/SinglePost.js
+++ src/components/singlePost/SinglePost.js
@@ -9,7 +9,20 @@ const singlePost = () => {
           src="https://images.pexels.com/photos/708392/pexels-photo-708392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
           alt=""
         />
-         <h1 className="singlePostTitle">Lorem ipsum dolor sit amet.</h1>
+        <h1 className="singlePostTitle">
+          Lorem ipsum dolor sit amet.
+          <div className="singlePostEdit">
+            <i className="singlePostIcon far fa-edit"></i>
+            <i className="singlePostIcon far fa-trash-alt"></i>
+          </div>
+        </h1>
+
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">
+            Author: <b>Godswill</b>
+          </span>
+          <span className="singlePostDate">1 hour ago</span>
+        </div>
       </div>
     </div>
   );
