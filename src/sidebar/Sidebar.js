bc669e6477df30e8403062840bfc7bfb0568a2fd
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-23 11:46:40 +0100

    working on sidebar

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,11 +1,21 @@
-import './sidebar.css'
+import "./sidebar.css";
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            sidebar
-        </div>
-    )
-}
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
+          alt=""
+        />
+        <p>
+          Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
+          amet ex esse.Sunt eu ut nostrud id quis proident.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
