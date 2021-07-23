933efc407e867eb204c80e6e406c08adf7a547b5
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-23 12:20:36 +0100

    working on post

--- src/post/Post.js
+++ src/post/Post.js
@@ -1,11 +1,20 @@
-import './post.css'
+import "./post.css";
 
 const Post = () => {
-    return (
-        <div className="post">
-            
+  return (
+    <div className="post">
+        <img className="postImg" src="" alt="" />
+      <div className="postInfo">
+        <div className="postCats">
+          <span className="postCat">Music</span>
+          <span className="postCat">Life</span>
         </div>
-    )
-}
+        <span className="postTitle">Lorem ipsum dolor sit amet</span>
+        <hr />
+        <span className="postDate">1 hour ago</span>
+      </div>
+    </div>
+  );
+};
 
-export default Post
+export default Post;
--- src/posts/Posts.js
+++ src/posts/Posts.js
@@ -1,9 +1,15 @@
+import Post from '../post/Post'
 import React from './posts.css'
 
 const Posts = () => {
     return (
         <div className="posts">
-            posts
+           <Post/>
+           <Post/>
+           <Post/>
+           <Post/>
+           <Post/>
+           <Post/>
         </div>
     )
 }
