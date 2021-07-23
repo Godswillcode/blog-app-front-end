a67ffa0d104bf2933122ee2bbfa3472c7049472e
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-23 12:25:20 +0100

    working on post

--- src/post/Post.js
+++ src/post/Post.js
@@ -3,7 +3,7 @@ import "./post.css";
 const Post = () => {
   return (
     <div className="post">
-        <img className="postImg" src="" alt="" />
+        <img className="postImg" src="https://images.pexels.com/photos/708392/pexels-photo-708392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
       <div className="postInfo">
         <div className="postCats">
           <span className="postCat">Music</span>
