5994978f57e905d25417dad3e6ae31f5f43e0540
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-24 01:40:04 +0100

    working on singlepost

--- src/components/singlePost/SinglePost.js
+++ src/components/singlePost/SinglePost.js
@@ -23,6 +23,36 @@ const singlePost = () => {
           </span>
           <span className="singlePostDate">1 hour ago</span>
         </div>
+        <p className="singlePostDesc">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
+          quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
+          Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
+          eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+          error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
+          impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
+          odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos!
+          <br />
+          <br />
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
+          quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
+          Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
+          eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+          error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
+          impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
+          odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos! Lorem, ipsum dolor sit amet consectetur.
+        </p>
       </div>
     </div>
   );
