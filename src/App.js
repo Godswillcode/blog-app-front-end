e7e415fa6c0193e4b45c99ca08313552736a807c
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-24 12:54:09 +0100

    working on route

--- src/App.js
+++ src/App.js
@@ -4,13 +4,33 @@ import Single from "./Pages/single/Single";
 import Write from "./Pages/write/Write";
 import Settings from "./Pages/settings/Settings";
 import Login from "./Pages/login/Login";
+import Register from "./Pages/register/Register";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-   <>
-  <Topbar/>
-  <Login/>
-   </>
+    <Router>
+      <Topbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">{user ? <Home /> : <Register />}</Route>
+
+        <Route path="/login">{user ? <Home /> : <Login />}</Route>
+
+        <Route path="/write">{user ? <Write /> : <Register />}</Route>
+
+        <Route path="/settings">
+          <Settings />
+        </Route>
+
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
