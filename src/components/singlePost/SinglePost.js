ad2091badb9d0e0432010073bcb4c53fb3a7114c
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-24 01:52:42 +0100

    working on write

--- src/components/singlePost/SinglePost.js
+++ src/components/singlePost/SinglePost.js
@@ -40,8 +40,6 @@ const singlePost = () => {
           elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
           iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
           a odit modi eos!
-          <br />
-          <br />
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
           quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
           Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
