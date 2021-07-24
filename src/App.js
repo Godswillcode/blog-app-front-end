c8af2c2e2fce2f5e89f1283c8ea4afd83db65554
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-24 02:14:19 +0100

    working on settings

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import Home from "./Pages/home/Home";
 import Topbar from "./components/topbar/Topbar";
 import Single from "./Pages/single/Single";
 import Write from "./Pages/write/Write";
-import Setting from "./Pages/settings/Setting";
+import Settings from "./Pages/settings/Settings";
 
 function App() {
   return (
    <>
   <Topbar/>
-  <Setting/>
+  <Settings/>
    </>
   );
 }
---
+++ src/Pages/settings/Settings.js
@@ -0,0 +1,13 @@
+import Sidebar from '../../components/sidebar/Sidebar'
+import './settings.css'
+
+const Settings = () => {
+    return (
+        <div className="settings">
+            <div className="settingsWrapper"></div>
+            <Sidebar/>
+        </div>
+    )
+}
+
+export default Settings
