d4febdf513d898d594d58e541a7aa394d18d27f0
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-24 02:26:16 +0100

    working on settings

--- src/Pages/settings/Settings.js
+++ src/Pages/settings/Settings.js
@@ -14,7 +14,7 @@ const Settings = () => {
           <div className="settingsPP">
             <img
               src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-              alt=""
+              alt="image"
             />
             <label htmlFor="fileInput">
               <i className="settingsPPIcon far fa-user-circle"></i>{" "}
