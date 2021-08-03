dd0b95b6b26985aaeeed6aefd4dc75c75a845120
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-08-03 11:07:03 +0100

    working on api

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,16 +4,21 @@ const User = require("../models/User");
 // REGISTER
 router.post("/register", async (req, res) => {
   try {
+    // HASH PASSWORD
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPassword,
     });
 
     const user = await newUser.save();
+    res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
 
 module.exports = router;
