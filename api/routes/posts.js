28563e0f5f869dcad8aea6bde9461c7fd86cdfa1
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-08-03 12:08:33 +0100

    working on api

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -12,3 +12,28 @@ router.post("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// UPDATE POST
+router.put("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.username === req.body.username) {
+        try {
+          const updatedPost = await Post.findByIdAndUpdate(
+            req.params.id,
+            {
+              $set: req.body,
+            },
+            { new: true }
+          );
+          res.status(200).json(updatedPost);
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      } else {
+        res.status(401).json("You can update only your post!");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
